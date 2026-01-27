e98524a5369b5b939ed54c68ada26591065552bb
Skolist <tech@skolist.com>
2026-01-27 19:08:35 +0530

    Removed old comments

--- apps/ai_paper_generator/src/App.tsx
+++ apps/ai_paper_generator/src/App.tsx
@@ -1,4 +1,3 @@
-// import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ProtectedRoute } from "@skolist/auth";
 import { DashboardPage } from "./pages/dashboard";
