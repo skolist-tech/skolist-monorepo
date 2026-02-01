c6159f5a709d96912a76de550f26105349d1c5e7
Skolist <tech@skolist.com>
2026-02-02 00:21:55 +0530

    Added Vercel Analytics in qgen , and switched to vercel next analytics in landing page

--- apps/ai_paper_generator/src/main.tsx
+++ apps/ai_paper_generator/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/next"
 
 import { AuthProvider } from "@skolist/auth";
 import { Toaster } from "@skolist/ui";
@@ -24,5 +24,6 @@ createRoot(document.getElementById("root")!).render(
         </AuthProvider>
       {/* </QueryClientProvider> */}
     </BrowserRouter>
+    <Analytics />
   </StrictMode>
 );
